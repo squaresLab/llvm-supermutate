2b333be254dd1dfc761a05ba0d7643a69a2818e3
ChrisTimperley <christimperley@googlemail.com>
2022-02-28 11:44:40 -0500

    added llvmsupermutate wrapper script

--- example/run.sh
+++ example/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 IMAGE_NAME="llvm-supermutate/examples:foo"
 VOLUME_LLVM="llvm11_opt"
 VOLUME_SUPERMUTATE="llvmsupermutate_opt"
+
+pushd "${HERE_DIR}"
 docker build -t "${IMAGE_NAME}" .
 docker run \
   --rm \
