289d0be2e588714a4a1ae0a2dad13cdf31999a39
ChrisTimperley <christimperley@googlemail.com>
2022-02-27 21:19:15 -0500

    generate .bc and .ll for example

---
+++ example/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+DIR_EXAMPLE="/example"
+
+export LLVM11_DIR="/opt/llvm11"
+export LLVMSUPERMUTATE_DIR="/opt/llvmsupermutate"
+export PATH="${LLVMSUPERMUTATE_DIR}/bin:${LLVM11_DIR}/bin:${PATH}"
+
+pushd "${DIR_EXAMPLE}"
+
+# generate .bc and .ll
+clang-11 -emit-llvm -o foo.bc -c foo.c
+llvm-dis -o foo.ll foo.bc
+
+exec $@
