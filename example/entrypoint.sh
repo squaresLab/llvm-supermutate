a499faaa0b115dd6704ba79267317d93e34a5802
ChrisTimperley <christimperley@googlemail.com>
2022-02-28 11:19:10 -0500

    switched Docker build to C++14 to avoid runtime linker issues

--- example/entrypoint.sh
+++ example/entrypoint.sh
@@ -7,6 +7,8 @@ export LLVM11_DIR="/opt/llvm11"
 export LLVMSUPERMUTATE_DIR="/opt/llvmsupermutate"
 export PATH="${LLVMSUPERMUTATE_DIR}/bin:${LLVM11_DIR}/bin:${PATH}"
 
+export LD_LIBRARY_PATH="${LLVMSUPERMUTATE_DIR}/lib:${LLVM11_DIR}/lib:${LD_LIBRARY_PATH:-}"
+
 pushd "${DIR_EXAMPLE}"
 
 # generate .bc and .ll
