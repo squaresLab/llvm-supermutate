1b07f2e3f93ce6346b7d24b5393dc75338c8800d
ChrisTimperley <christimperley@googlemail.com>
2022-04-28 21:08:12 -0400

    added transitivePredecessors and findReachingValues helpers

---
+++ include/llvm-supermutate/Utils.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <set>
+#include <queue>
+
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/CFG.h>
+
+
+namespace llvmsupermutate {
+
+std::set<llvm::BasicBlock*> transitivePredecessors(llvm::BasicBlock* block) {
+  std::set<llvm::BasicBlock*> blocks;
+
+  std::queue<llvm::BasicBlock*> q;
+  for (auto *predecessor : llvm::predecessors(block)) {
+    q.push(predecessor);
+  }
+
+  while (!q.empty()) {
+    auto *predecessor = q.front();
+    q.pop();
+
+    if (blocks.find(predecessor) != blocks.end()) {
+      continue;
+    }
+
+    blocks.insert(predecessor);
+    for (auto *transitive : llvm::predecessors(predecessor)) {
+      q.push(transitive);
+    }
+  }
+
+  return blocks;
+}
+
+std::set<llvm::Value*> findReachingValues(llvm::Instruction *before) {
+  std::set<llvm::Value*> values;
+  auto *function = before->getFunction();
+  auto *block = before->getParent();
+  auto *module = before->getModule();
+
+  // formals
+  for (auto &argument : function->args()) {
+    values.insert(&argument);
+  }
+
+  // globals
+  for (auto &global : module->globals()) {
+    values.insert(&global);
+  }
+
+  // instructions in the same block
+  for (auto &instruction : *block) {
+    if (&instruction == before) {
+      break;
+    }
+    values.insert(&instruction);
+  }
+
+  // find all instructions in transitive predecessor blocks
+  // NOTE dominator tree may be needed; this may miss some instructions
+  for (auto *predecessor : transitivePredecessors(block)) {
+    for (auto &instruction : *predecessor) {
+      values.insert(&instruction);
+    }
+  }
+
+  return values;
+}
+
+}
